aac4646f7117042283f32a42b979983447706524
rainfordays <sebb.tornquist@gmail.com>
2020-01-09 15:52:17 +0100

    Update AutoDeleteItems.lua

--- AutoDeleteItems.lua
+++ AutoDeleteItems.lua
@@ -6,7 +6,12 @@ end
 
 
 core.prefix = "|cff42adf5/ad|r "
+core.addonName = "|cff42adf5AutoDeleteItems|r "
 
+
+--[[
+    ---- EVENT FRAME ----
+]]
 local events = CreateFrame("Frame");
 events:RegisterEvent("ADDON_LOADED");
 events:RegisterEvent("VARIABLES_LOADED");
@@ -16,6 +21,9 @@ events:SetScript("OnEvent", function(self, event, ...)
 end)
 
 
+--[[
+    -- ADDON LOADED --
+]]
 function events:ADDON_LOADED(name)
   if name ~= "AutoDeleteItems" then return end
   if AutoDeleteItems == nil then AutoDeleteItems = {} end
@@ -28,25 +36,35 @@ function events:ADDON_LOADED(name)
   end
 end
 
+
+
+--[[
+    -- VARIABLES LOADED --
+]]
 function events:VARIABLES_LOADED()
 
-  StaticPopupDialogs["DELETE_ITEM"].button3 = "Auto-delete"
-  StaticPopupDialogs["DELETE_ITEM"].OnAlt = function(self)
-    local infoType, itemID, itemLink = GetCursorInfo()
-    AutoDeleteItems[itemLink] = true
-    DeleteCursorItem()
+  StaticPopupDialogs["DELETE_ITEM"].button3 = "Auto-delete" -- Add third button to the delete item popup
+  StaticPopupDialogs["DELETE_ITEM"].OnAlt = function(self) -- Add function for third button
+    local infoType, itemID, itemLink = GetCursorInfo() -- Get cursor item info
+    AutoDeleteItems[itemLink] = true -- Add item to autodelete
+    DeleteCursorItem() -- Delete cursor item
   end
 
+  core:Print(core.addonName .. "loaded. /ad for more information.")
 end
 
 
-function events:BAG_UPDATE_DELAYED()
 
+--[[
+    -- BAG UPDATE DELAYED --
+    (WE WANT THE DELAYED ONE BECAUSE IT FIRES LAST)
+]]
+function events:BAG_UPDATE_DELAYED()
   for bag = 0, NUM_BAG_SLOTS do
     for slot = 1, GetNumBagSlots(bag) do
       local itemLink = GetContainerItemLink(bag, slot)
       if itemLink then
-        if AutoDelete[itemLink] then
+        if AutoDeleteItems[itemLink] then
           PickupContainerItem(bag, slot)
           DeleteCursorItem()
         end
@@ -58,30 +76,32 @@ end
 
 
 
-
-
+--[[
+    ---- SLASH COMMANDS ----
+]]
 function core:SlashCommand(args)
-  local command, itemLinkChat, rest = strsplit(" ", args, 3)
-  command = command:lower()
+  local command, itemLinkChat, rest = strsplit(" ", args, 3) -- Split args to a command and itemlink
+  command = command:lower() -- command to lowercase for easier detection
 
   if command == "save" then
-    local itemName = GetItemInfo(itemLinkChat)
+    local itemName = GetItemInfo(itemLinkChat) -- Make sure the subcommand is an actual itemlink
     if itemName then
-      for itemLink, _ in pairs(AutoDelete) do
-        if itemLink == itemLinkChat then
-          AutoDelete[itemLink] = nil
+      for itemLink, _ in pairs(AutoDeleteItems) do -- Loop through autodelete items
+        if itemLink == itemLinkChat then -- if match is found
+          AutoDeleteItems[itemLink] = nil -- delete table entry, (making the addon NOT delete that item)
           return
         end
       end
     end
   elseif command == "delete" then
-    local itemName = GetItemInfo(itemLinkChat)
+    local itemName = GetItemInfo(itemLinkChat) -- Make sure the subcommand is an actual itemlink
     if itemName then
-      AutoDelete[itemLinkChat] = true
+      AutoDeleteItems[itemLinkChat] = true -- Add item to autodelete
     end
   else
+    core:Print(core.addonName .. ": when you try deleting an item there will be a new button which will add that item to the auto-delete list. Other commands are listed below.")
     core:Print("Available commands")
     core:Print(core.prefix.."delete [item link]: delete the linked item")
-    core:Print(core.prefix.."save [item link]: do not delete the linked item")
+    core:Print(core.prefix.."save [item link]: do NOT delete the linked item")
   end
 end
